bc00f95753421ccce7ae2de746ec9d64fac57c52
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-05-09 19:42:06 +0530

    UC4:-Cricket League Data Sorted By SR With Fours And Sixes

--- src/main/java/com/bl/cricketleague/CricketLeague.java
+++ src/main/java/com/bl/cricketleague/CricketLeague.java
@@ -36,6 +36,15 @@ public class CricketLeague {
         return numberOfEnteries;
     }
 
+    public String getSortedStrikeRate6s4s()  {
+        if(censusCSVList.size()==0 || censusCSVList==null)
+            throw new CricketLeagueException(CricketLeagueException.ExceptionType.NO_CENSUS_DATA,"No Data");
+        Comparator<IplMostRuns> iplMostRunsComparator = Comparator.comparing(census -> census.SR);
+        this.sort(iplMostRunsComparator);
+        String sortedCensusJson = new Gson().toJson(censusCSVList);
+        return sortedCensusJson;
+    }
+
     public String getSortedWiseFours()  {
         if(censusCSVList.size()==0 || censusCSVList==null)
             throw new CricketLeagueException(CricketLeagueException.ExceptionType.NO_CENSUS_DATA,"No Data");
